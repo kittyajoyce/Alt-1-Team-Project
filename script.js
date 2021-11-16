4fc51f6e5b553703ac5ec63332c8e9c095699e23
kittyaj <>
2021-11-16 18:11:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+var elig = "";
+
+function yesnoCheck() {
+    if (document.getElementById("yes").checked) {
+        document.getElementById('ifYes').style.visibility = 'visible';
+        elig = "Yes";
+        sd = 5;
+    } else if (document.getElementById("no").checked) {
+      document.getElementById('ifYes').style.visibility = 'hidden';
+      elig = "No";
+      sd = 0;
+    }
+}
+
+function display_details() {
+  
+  document.getElementById('first').style.display = 'none';document.getElementById('second').style.display = '';
+  
+  document.getElementById('third').style.display = 'none';document.getElementById('fourth').style.display = '';
+
+  document.getElementById('5').style.display = 'none';document.getElementById('6').style.display = '';
+
+  var firstname = document.getElementById("firstname").value;
+  var lastname = document.getElementById("lastname").value;
+
+  var thedate = document.getElementById("thedate").value;
+
+  var cq = document.getElementById("childquantity").value;
+
+  var aq = document.getElementById("adultquantity").value;
+
+  var price = cq*10 + aq*20 -sd;
+
+/*
+  document.getElementById("order").innerHTML = "You ordered " + cq + " under 18 tickets and " + aq + " adult tickets for " + thedate; */
+
+  document.getElementById("fullname").innerHTML = "Name: " + firstname + " " + lastname;
+  
+  document.getElementById("date").innerHTML = "Date: " + thedate;
+
+  document.getElementById("discount").innerHTML =
+  "Eligible for €5 Student/Teacher Discount: " + elig;
+
+  document.getElementById("total").innerHTML = "Total: €" + price;
+
+}
+
+
