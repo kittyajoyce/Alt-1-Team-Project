1af6f0c14edac63db22e994e74fdab887680fff2
Kitty Joyce <75419859+kittyajoyce@users.noreply.github.com>
2021-11-19 18:58:54 +0000

    finishing

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 var elig = "";
+var sd = "";
+
 
 function yesnoCheck() {
     if (document.getElementById("yes").checked) {
@@ -13,37 +15,38 @@ function yesnoCheck() {
     }
 }
 
-function display_details() {
-  
-  document.getElementById('first').style.display = 'none';document.getElementById('second').style.display = '';
-  
-  document.getElementById('third').style.display = 'none';document.getElementById('fourth').style.display = '';
 
-  document.getElementById('5').style.display = 'none';document.getElementById('6').style.display = '';
+function display_details() {
+
+  document.getElementById('first').style.display = 'none';
+  document.getElementById('second').style.display = '';
+  
+  document.getElementById('third').style.display = 'none';
+  document.getElementById('fourth').style.display = '';
+
+  document.getElementById('5').style.display = 'none';
+  document.getElementById('6').style.display = '';
 
   var firstname = document.getElementById("firstname").value;
   var lastname = document.getElementById("lastname").value;
-
+  
   var thedate = document.getElementById("thedate").value;
 
   var cq = document.getElementById("childquantity").value;
-
   var aq = document.getElementById("adultquantity").value;
-
   var price = cq*10 + aq*20 -sd;
+  
+  var maininterest = document.getElementById("maininterest").value;
 
-/*
-  document.getElementById("order").innerHTML = "You ordered " + cq + " under 18 tickets and " + aq + " adult tickets for " + thedate; */
+  document.getElementById("order").innerHTML = "You ordered " + cq + " children tickets and " + aq + " adult tickets for this date:  " + thedate;
 
   document.getElementById("fullname").innerHTML = "Name: " + firstname + " " + lastname;
-  
-  document.getElementById("date").innerHTML = "Date: " + thedate;
 
-  document.getElementById("discount").innerHTML =
-  "Eligible for €5 Student/Teacher Discount: " + elig;
+  document.getElementById("mainint").innerHTML = "Main Interest: " + maininterest;
 
-  document.getElementById("total").innerHTML = "Total: €" + price;
+  document.getElementById("discount").innerHTML = "Eligible for €5 Student/Teacher Discount: " + elig;
 
+  document.getElementById("total").innerHTML = "TOTAL:  €" + price;
 }
 
-
+ 
